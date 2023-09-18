d100a6c837899d0ee4b645612734b546d9b5bbaa
luukhfontys <l.hooymans@student.fontys.nl>
2023-09-18 14:39:18 +0200

    mSghef

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print('123')
+import pandas as pd
+
+planning = pd.read_excel('omloop planning.xlsx')
+
+class bus:
+    def __init__(self, rit, locatie, batterij):
+        self.rit = rit
+        self.locatie = locatie
+        self.batterij = batterij
+    def __repr__(self) -> str:
+        return print(f'Rit: {self.rit}\nLocatie: {self.locatie}\nBatterij: {self.batterij} ')
