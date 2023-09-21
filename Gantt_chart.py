7c0697ae0a7c40f1083a9d762c8d4092dbde7cb7
MilouSloendregt <m.sloendregt@student.fontys.nl>
2023-09-21 12:53:06 +0200

    help

--- Gantt_chart.py
+++ Gantt_chart.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import pandas as pd
+import plotly.express as px
 
 df_planning = pd.read_excel('omloop planning.xlsx')
 df = pd.read_excel('omloop planning.xlsx')
@@ -38,38 +39,4 @@ df["eindtijd datum"] = pd.to_datetime(df["eindtijd datum"])
 
 df['buslijn'] = df['buslijn'].fillna(999)
 
-
-
-
-
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import pandas as pd
-
-df["starttijd datum"] = pd.to_datetime(df["starttijd datum"])
-df["eindtijd datum"] = pd.to_datetime(df["eindtijd datum"])
-
-def create_gantt_chart(df, buslijn):
-    fig, ax = plt.subplots(figsize=(12, 6))
-
-    for index, row in df.iterrows():
-        ax.barh(
-            y=row["activiteit"],
-            left=row["starttijd datum"],
-            width=row["eindtijd datum"] - row["starttijd datum"],
-            align="center"
-        )
-
-    ax.xaxis.set_major_locator(mdates.HourLocator(interval=1))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%H:%M:%S"))
-    ax.set_xlabel("Tijd")
-    ax.set_ylabel("Activiteit")
-    ax.set_title(f"Gantt-diagram voor Buslijn {buslijn}")
-    plt.legend()
-
-    plt.tight_layout()
-    plt.show()
-
-for buslijn in df["buslijn"].unique():
-    df_buslijn = df[df["buslijn"] == buslijn]
-    create_gantt_chart(df_buslijn, buslijn)
+fig = px.timeline(df, x_start="starttijd datum", x_end="eindtijd datum", y="omloopnummer")
---
+++ help.py
@@ -0,0 +1,7 @@
+from Functions import prestatiemaat_materiaal_minuten
+import pandas as pd
+from bus_class import bus
+from minimale_oplaadtijd import minimale_oplaadtijd
+
+df = pd.read_excel('omloop planning.xlsx')
+
