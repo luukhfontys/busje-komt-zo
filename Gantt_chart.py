700107ae276e46f919f42d2c7dc79f49d2f46648
luukhfontys <l.hooymans@student.fontys.nl>
2023-09-27 15:57:39 +0200

    streamlit update

--- Gantt_chart.py
+++ Gantt_chart.py
@@ -4,49 +4,52 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.express as px
 
-df_planning = pd.read_excel('omloop planning.xlsx')
-df = pd.read_excel('omloop planning.xlsx')
+def Gantt_chart(omloop_planning: pd.DataFrame):
+    """"Vul path naar omloop planning in, output: een plotly gantt chart object."""
+    df_planning = omloop_planning
+    df = omloop_planning
 
-starttijd = df_planning.starttijd.values.tolist()
-eindtijd = df_planning.eindtijd.values.tolist()
-activiteit = df_planning.activiteit.values.tolist()
+    starttijd = df_planning.starttijd.values.tolist()
+    eindtijd = df_planning.eindtijd.values.tolist()
+    activiteit = df_planning.activiteit.values.tolist()
 
-index = 0
-begintijd = []
-lengte = []
+    index = 0
+    begintijd = []
+    lengte = []
 
-for i in activiteit:
-    start_time = starttijd[index]
-    end_time = eindtijd[index]
-    t2 = datetime.strptime(end_time, "%H:%M:%S")
-    t1 = datetime.strptime(start_time, "%H:%M:%S")
-    begintijd.append(t1)
-    delta = t2-t1
-    sec = delta.total_seconds()
-    minuten = sec/60
-    lengte.append(minuten)
+    for i in activiteit:
+        start_time = starttijd[index]
+        end_time = eindtijd[index]
+        t2 = datetime.strptime(end_time, "%H:%M:%S")
+        t1 = datetime.strptime(start_time, "%H:%M:%S")
+        begintijd.append(t1)
+        delta = t2-t1
+        sec = delta.total_seconds()
+        minuten = sec/60
+        lengte.append(minuten)
 
-    index += 1
-df['begintijd'] = begintijd
-df['lengte'] = lengte
-
-
-df["starttijd datum"] = pd.to_datetime(df["starttijd datum"])
-df["eindtijd datum"] = pd.to_datetime(df["eindtijd datum"])
-
-df.drop(df[df['activiteit'] == 'idle'].index, inplace = True)
-
-for index, row in df.iterrows():
-    if row['activiteit'] == 'dienst rit':
-        if row['buslijn'] == '400' or '401':
-            value = row['buslijn']
-            df.loc[index, ['activiteit']] = [value]
+        index += 1
+    df['begintijd'] = begintijd
+    df['lengte'] = lengte
 
 
-y_label = {'omloop nummer': 'Omloop nummer'}
+    df["starttijd datum"] = pd.to_datetime(df["starttijd datum"])
+    df["eindtijd datum"] = pd.to_datetime(df["eindtijd datum"])
 
-colors = {"materiaal rit": "forestgreen", "opladen": "darkgray", 401: "cornflowerblue", 400.0: "steelblue"}
+    df.drop(df[df['activiteit'] == 'idle'].index, inplace = True)
 
-fig = px.timeline(df, x_start="starttijd datum", x_end="eindtijd datum", y="omloop nummer", color = 'activiteit', title = "Gantt Chart", labels = y_label, color_discrete_map = colors)
-fig.update_layout(xaxis_title="Date")
-fig.show()
+    for index, row in df.iterrows():
+        if row['activiteit'] == 'dienst rit':
+            if row['buslijn'] == '400' or '401':
+                value = row['buslijn']
+                df.loc[index, ['activiteit']] = [value]
+
+
+    y_label = {'omloop nummer': 'Omloop nummer'}
+
+    colors = {"materiaal rit": "forestgreen", "opladen": "darkgray", 401: "cornflowerblue", 400.0: "steelblue"}
+
+    fig = px.timeline(df, x_start="starttijd datum", x_end="eindtijd datum", y="omloop nummer", color = 'activiteit', title = "Gantt Chart", labels = y_label, color_discrete_map = colors)
+    fig.update_layout(xaxis_title="Date")
+
+    return fig
--- Streamlittest.py
+++ Streamlittest.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
+from Gantt_chart import Gantt_chart
+import matplotlib.pyplot as plt
+import plotly.express as px
 
+
+# df_planning = pd.read_excel('omloop planning.xlsx')
+# fig = Gantt_chart(df_planning)
+# fig.show()
 st.title('Excel invoer')
 
 if st.button('Test'):
@@ -12,7 +19,9 @@ if st_omloop is not None:
     st.write(st_omloop.name)
     df_omloop = pd.read_excel(st_omloop)
     st.dataframe(df_omloop)
+    st.plotly_chart(Gantt_chart(df_omloop))
     
 if st_omloop is not None:
     st.write('Editable dataframe:')
     editor = st.data_editor(df_omloop, num_rows='dynamic')
+
