8979bd679dc42f1a7ef181ae57c5b67237e1baa0
luukhfontys <l.hooymans@student.fontys.nl>
2023-10-21 15:12:47 +0200

    generic usermanuel update

--- pdf.py
+++ pdf.py
@@ -11,7 +11,7 @@ st.set_page_config(
 
 st.title('Bussie on its way user manual')
 
-pdf_path = "User Manual.pdf"
+pdf_path = "User Manual generic.pdf"
 images = convert_from_path(pdf_path)
 
 for image in images:
