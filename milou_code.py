ffd3e242d6261958245d5d717d3ea236ab509ca8
Paul van Overveld <492276@student.fontys.nl>
2023-11-12 18:46:34 +0100

    yeeeye

--- milou_code.py
+++ milou_code.py
@@ -4,9 +4,12 @@ import numpy as np
 from Functie_to_class_format import check_dienstregeling
 
 #import
-dienstregeling = pd.read_excel('Connexxion data - 2023-2024.xlsx', sheet_name = 'Dienstregeling')
-afstand = pd.read_excel('Connexxion data - 2023-2024.xlsx', sheet_name = 'Afstand matrix')
-
+def importeren(bestandsnaam:str='Connexxion data - 2023-2024.xlsx', sheetnaam1:str='Dienstregeling', sheetnaam2:str='Afstand matrix'):
+    ''' importeerd de relevante data uit excelsheets
+    '''
+    dienstregeling = pd.read_excel(bestandsnaam, sheet_name = sheetnaam1)
+    afstand = pd.read_excel(bestandsnaam, sheet_name = sheetnaam2)
+    return dienstregeling, afstand
 
 def tijden_minuten(dienstregeling):
     '''Zet de vertrektijden om in minuten en voegt deze minuten toe aan het dataframe. Vervolgens worden 
@@ -31,29 +34,29 @@ def tijden_minuten(dienstregeling):
     return dienstregeling
 
 #Verbruik per kilometer ligt tussen de 0.7 en 2.5 kWh
-verbruik = []
-for index, row in dienstregeling.iterrows():
-    startlocatie = row['startlocatie']
-    eindlocatie = row['eindlocatie']
-    lijn = row['buslijn']
+# verbruik = []
+# for index, row in dienstregeling.iterrows():
+#     startlocatie = row['startlocatie']
+#     eindlocatie = row['eindlocatie']
+#     lijn = row['buslijn']
+#     for index, row in afstand.iterrows():
+#         eindlocatie2 = row['eindlocatie']
+#         startlocatie2 = row['startlocatie']
+#         lijn2 = row['buslijn']
+#         afstand_meter = row['afstand in meters']
+#         if startlocatie == startlocatie2 and eindlocatie == eindlocatie2 and lijn == lijn2:
+#             afstand_kilometer = afstand_meter / 1000
+#             verbruik.append(afstand_kilometer * 1.6)
+
+# dienstregeling['verbruik'] = verbruik
+def verbruik_afstands_matrix(afstand:pd.DataFrame)->pd.DataFrame:
+    verbruik_afstand = []
     for index, row in afstand.iterrows():
-        eindlocatie2 = row['eindlocatie']
-        startlocatie2 = row['startlocatie']
-        lijn2 = row['buslijn']
         afstand_meter = row['afstand in meters']
-        if startlocatie == startlocatie2 and eindlocatie == eindlocatie2 and lijn == lijn2:
-            afstand_kilometer = afstand_meter / 1000
-            verbruik.append(afstand_kilometer * 1.6)
-
-dienstregeling['verbruik'] = verbruik
-
-verbruik_afstand = []
-for index, row in afstand.iterrows():
-    afstand_meter = row['afstand in meters']
-    afstand_kilometer = afstand_meter / 1000
-    verbruik_afstand.append(afstand_kilometer * 1.6)
-
-afstand['verbruik'] = verbruik_afstand
+        afstand_kilometer = afstand_meter / 1000
+        verbruik_afstand.append(afstand_kilometer * 1.6)
+    afstand['verbruik'] = verbruik_afstand
+    return afstand
 
 def create_planning(dienstregeling:pd.DataFrame, afstand:pd.DataFrame,batterijwaarde:float=270)->list:
     ''' Maakt een planning op basis van de ingegeven dienstregeling en afstandsmatrix
@@ -83,9 +86,6 @@ def create_planning(dienstregeling:pd.DataFrame, afstand:pd.DataFrame,batterijwa
             bussen.append(nieuwe_bus)
             solved = nieuwe_bus.add_drive(Time=vertrektijd, First_location=start_locatie, Final_location= eind_locatie, Busline= buslijn)
     return bussen
-#print(afstand)
-#print(bussen[3].schedule)
-#print(len(bussen))
 
 def oplossing_uitlezen(bussen):
     '''Zet de gemaakte oplossing om in lijsten voor het maken van een planning'''
@@ -106,7 +106,7 @@ def oplossing_uitlezen(bussen):
 
     return startlocatie_lijst, eindlocatie_lijst, buslijnen, begintijden, omlopen 
 
-def starttijden_goedzetten(begintijden):
+def starttijden_goedzetten(begintijden, datum_morgen, datum_vandaag):
     '''Zet de lijst met minuten terug in het goede format van uren en minuten
     Args: begintijden(lijst) met tijd in minuten
     Returns: startijden(lijst) met tijd in uren, minuten
@@ -126,8 +126,7 @@ def starttijden_goedzetten(begintijden):
         starttijd = f'{uren}:{minuten}:00'
         starttijden.append(starttijd)
 
-    datum_vandaag = '11-13-2023'
-    datum_morgen = '11-14-2023'
+
 
     datums = []
 
@@ -174,18 +173,38 @@ def activiteit_buslijn(buslijnen):
         else:
             activiteiten.append('idle')
             buslijn.append(np.nan)
+    return activiteiten
 
-def begin_planning(startlocatie_lijst, eindlocatie_lijst, starttijden, activiteiten, buslijn):
+def begin_planning(bussen, afstand, dienstregeling,datum_morgen, datum_vandaag):
     '''Maakt het eerste deel van de nieuwe planning aan'''
+    startlocatie_lijst, eindlocatie_lijst, buslijn, begintijden, omlopen = oplossing_uitlezen(bussen)
+    starttijden, datums = starttijden_goedzetten(begintijden, datum_morgen, datum_vandaag)
+    activiteiten = activiteit_buslijn(buslijn)
     nieuwe_planning = pd.DataFrame()
     nieuwe_planning['startlocatie'] = startlocatie_lijst
     nieuwe_planning['eindlocatie'] = eindlocatie_lijst
     nieuwe_planning['starttijd'] = starttijden
     nieuwe_planning['activiteit'] = activiteiten
     nieuwe_planning['buslijn'] = buslijn
-    return nieuwe_planning
+    verbruik = verbruik_matrix(nieuwe_planning, afstand)
+    duur = lengte_rit(nieuwe_planning, afstand)
+    eindtijden, datums_eind = eindtijden_goedzetten(begintijden, duur, datum_morgen, datum_vandaag)
+    nieuwe_planning['eindtijd'] = eindtijden
+    nieuwe_planning['energieverbruik'] = verbruik
+    nieuwe_planning['starttijd datum'] = datums
+    nieuwe_planning['eindtijd datum'] = datums_eind
+    nieuwe_planning['omloop nummer'] = omlopen
 
-def verbruik_matrix(nieuwe_planning):
+    cols = nieuwe_planning.columns.tolist()
+    cols = ['startlocatie', 'eindlocatie', 'starttijd', 'eindtijd', 'activiteit', 'buslijn', 'energieverbruik', 'starttijd datum', 'eindtijd datum', 'omloop nummer']
+    df = nieuwe_planning[cols]
+
+    df[['starttijd datum', 'eindtijd datum']] = df[['starttijd datum', 'eindtijd datum']].apply(pd.to_datetime)
+
+    df.to_excel('OmloopplanningIdleop2Min.xlsx')
+    return
+
+def verbruik_matrix(nieuwe_planning, afstand):
     '''Kijkt naar alle ritten uit de planning en zoekt uit de afstandsmatrix het juiste verbruik erbij
     Args: nieuwe_planning(DataFrame)
     Return: verbruik(lijst)'''
@@ -214,10 +233,11 @@ def verbruik_matrix(nieuwe_planning):
         elif rit == 'Opladen':
             verbruik.append(-225.0/2)
         else:
-            print(rit)
-            print('foutcode')
+            ''
+            #print('foutcode')
+    return verbruik
 
-def lengte_rit(nieuwe_planning):
+def lengte_rit(nieuwe_planning, afstand):
     '''Kijkt naar alle ritten uit de planning en zoekt uit de afstandsmatrix het juist aantal minuten erbij
     Args: nieuwe_planning(DataFrame)
     Return: Duur(lijst) met aantal minuten per rit'''
@@ -246,11 +266,12 @@ def lengte_rit(nieuwe_planning):
         elif rit == 'Opladen':
             duur.append(15)
         else:
-            print(rit)
-            print('foutcode')
+            ' '
+            #print('foutcode')
+            
+    return duur
 
-
-def eindtijden_goedzetten(begintijden, duur):
+def eindtijden_goedzetten(begintijden, duur, datum_morgen, datum_vandaag):
     '''Voegt bij de starttijden het aantal met minuten van de rit toe. Zet de lijst met minuten terug 
     in het goede format van uren en minuten
     Args: begintijden(lijst) met tijd in minuten
@@ -301,27 +322,32 @@ def eindtijden_goedzetten(begintijden, duur):
             datums_eind.append(dag_vandaag)
     return eindtijden, datums_eind
 
-def planning_deel2(nieuwe_planning, eindtijden, verbruik, datums, datums_eind, omlopen):
-    nieuwe_planning['eindtijd'] = eindtijden
-    nieuwe_planning['energieverbruik'] = verbruik
-    nieuwe_planning['starttijd datum'] = datums
-    nieuwe_planning['eindtijd datum'] = datums_eind
-    nieuwe_planning['omloop nummer'] = omlopen
+# def planning_compleet(bussen,afstand, eindtijden, datums_eind):
+#     nieuwe_planning, omlopen, datums = begin_planning(bussen)
+#     verbruik = verbruik_matrix(nieuwe_planning, afstand)
+#     eindtijden_goedzetten()
+#     nieuwe_planning['eindtijd'] = eindtijden
+#     nieuwe_planning['energieverbruik'] = verbruik
+#     nieuwe_planning['starttijd datum'] = datums
+#     nieuwe_planning['eindtijd datum'] = datums_eind
+#     nieuwe_planning['omloop nummer'] = omlopen
 
-    cols = nieuwe_planning.columns.tolist()
-    cols = ['startlocatie', 'eindlocatie', 'starttijd', 'eindtijd', 'activiteit', 'buslijn', 'energieverbruik', 'starttijd datum', 'eindtijd datum', 'omloop nummer']
-    df = nieuwe_planning[cols]
+#     cols = nieuwe_planning.columns.tolist()
+#     cols = ['startlocatie', 'eindlocatie', 'starttijd', 'eindtijd', 'activiteit', 'buslijn', 'energieverbruik', 'starttijd datum', 'eindtijd datum', 'omloop nummer']
+#     df = nieuwe_planning[cols]
 
-    df[['starttijd datum', 'eindtijd datum']] = df[['starttijd datum', 'eindtijd datum']].apply(pd.to_datetime)
+#     df[['starttijd datum', 'eindtijd datum']] = df[['starttijd datum', 'eindtijd datum']].apply(pd.to_datetime)
 
-    print(len(bussen))
+#     df.to_excel('OmloopplanningIdleop2Min.xlsx')
 
-    #df.to_excel('OmloopplanningIdleop2Min.xlsx')
+def main():
+    datum_vandaag = '11-13-2023'
+    datum_morgen = '11-14-2023'
+    dienstregeling, afstand = importeren()
+    afstand = verbruik_afstands_matrix(afstand=afstand)
+    dienstregeling = tijden_minuten(dienstregeling)
+    bussen = create_planning(dienstregeling, afstand)
+    begin_planning(bussen,afstand,dienstregeling,datum_morgen,datum_vandaag)
 
-
-def uitvoeren():
-    tijden_minuten(dienstregeling)
-    create_planning(dienstregeling, afstand,270)
-    oplossing_uitlezen(bussen)
-
-uitvoeren()
+if __name__ == '__main__':
+    main()
